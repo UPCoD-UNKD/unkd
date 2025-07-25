6bb12b1622a8101935ea912e0b8abf00698072d9
Golubev_E <92272740+GitHubByGolubev@users.noreply.github.com>
2025-07-25 14:17:28 +0300

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import astroIcon from "astro-icon";
+
+export default defineConfig({
+  site: "http://localhost:4321",
+  integrations: [
+    tailwind(),
+    astroIcon({
+      iconSets: ["bx"], 
+    }),
+  ],
+});
